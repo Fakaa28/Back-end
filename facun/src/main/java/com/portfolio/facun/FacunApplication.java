12248e4bd63676e6992d479122aa928ed4987cb5
Facundo <fakamadafaka_2004@hotmail.com>
2023-06-20 19:30:10 -0300

    2da entrega

---
+++ facun/src/main/java/com/portfolio/facun/FacunApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.facun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacunApplication.class, args);
+	}
+
+}
